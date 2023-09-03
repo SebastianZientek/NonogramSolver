a6ec87ff4c5915247a85f37ccc1feaa5488f8c87
Sebastian Zientek <sebastian.zientek1@gmail.com>
2023-09-03 15:41:15 +0200

    Working prototype

---
+++ src/ColumnIterator.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <iterator>
+
+template <typename T, typename IT>
+struct ColumnIterator
+{
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = T;
+    using pointer = T *;
+    using reference = T &;
+
+    ColumnIterator(IT it, size_t width)
+        : m_it(it)
+        , m_width(width)
+    {
+    }
+
+    reference operator*() const
+    {
+        return *m_it;
+    }
+    pointer operator->()
+    {
+        return m_it;
+    }
+
+    ColumnIterator &operator++()
+    {
+        m_it += m_width;
+        return *this;
+    }
+
+    ColumnIterator operator++(int)
+    {
+        ColumnIterator tmp = *this;
+        m_it += m_width;
+        return tmp;
+    }
+
+    friend bool operator==(const ColumnIterator &a, const ColumnIterator &b)
+    {
+        return a.m_it == b.m_it;
+    };
+    friend bool operator!=(const ColumnIterator &a, const ColumnIterator &b)
+    {
+        return a.m_it != b.m_it;
+    };
+
+private:
+    IT m_it;
+    size_t m_width;
+    difference_type m_offset;
+    difference_type m_column;
+    difference_type m_columnOffset;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <array>
 #include <bitset>
 #include <initializer_list>
@@ -6,6 +7,7 @@
 #include <span>
 #include <vector>
 
+#include "ColumnIterator.hpp"
 #include "Combination.hpp"
 #include "Generator.hpp"
 #include "Line.hpp"
@@ -13,36 +15,124 @@
 
 int main(int argc, char **argv)
 {
+    // std::vector<std::vector<SideNumberType>> sideRows{
+    //     {3, 3}, {2, 4, 2}, {1, 2, 1}, {1, 1}, {2, 2}, {3, 3}, {3, 3}, {6}, {4}, {2},
+    // };
+
+    // std::vector<std::vector<SideNumberType>> sideCols{
+    //     {5}, {2, 3}, {1, 3}, {2, 3}, {2, 3}, {2, 3}, {2, 3}, {1, 3}, {2, 3}, {5},
+    // };
+
     std::vector<std::vector<SideNumberType>> sideRows{
-        {3, 3}, {2, 4, 2}, {1, 2, 1}, {1, 1}, {2, 2}, {3, 3}, {3, 3}, {6}, {4}, {2},
+        {5},
+        {3, 3},
+        {2, 5},
+        {1, 4, 2},
+        {5, 2},
+        {9, 1},
+        {3, 4},
+        {1, 2, 4, 1},
+        {4, 5, 2},
+        {3, 4, 2},
+        {2, 2, 1},
+        {5, 3, 1},
+        {3, 4, 3},
+        {6, 2},
+        {4, 4, 1},
+        {1, 1, 2, 1, 1},
+        {3, 1, 1},
+        {2, 8},
+        {2},
+        {10}
     };
 
     std::vector<std::vector<SideNumberType>> sideCols{
-        {5}, {2, 3}, {1, 3}, {2, 3}, {2, 3}, {2, 3}, {2, 3}, {1, 3}, {2, 3}, {5},
+        {2, 3},
+        {2, 4},
+        {1, 10},
+        {2, 5, 6},
+        {1, 3, 4, 3},
+        {1, 3, 3, 7},
+        {1, 4, 3, 1, 1},
+        {6, 2, 2, 3, 1},
+        {3, 1, 3, 1, 2, 1, 1},
+        {2, 1, 2, 1, 1, 1, 1},
+        {2, 4, 2, 1, 1, 1},
+        {2, 2, 1, 2, 1, 1},
+        {3, 2, 1, 1},
+        {4, 2, 1, 1},
+        {10, 1}
     };
 
-    Line line({4, 3}, 10);
+    std::vector<Line> rows;
+    std::vector<Line> cols;
 
-    std::vector<CellType> painted(15);
-    line.paintKnownFields(painted.begin());
+    std::for_each(sideRows.begin(), sideRows.end(),
+                  [&rows, &sideCols](auto &sideRow)
+                  { rows.emplace_back(std::move(sideRow), sideCols.size()); });
 
-    for (const auto &seq : painted)
+    std::for_each(sideCols.begin(), sideCols.end(),
+                  [&cols, &sideRows](auto &sideCol)
+                  { cols.emplace_back(std::move(sideCol), sideRows.size()); });
+
+    std::vector<CellType> data(rows.size() * cols.size(), E);
+
+    while (true)
     {
-        switch (seq)
         {
-        case E:
-            std::cout << '_';
-            break;
-        case B:
-            std::cout << 'X';
-            break;
-        case W:
-            std::cout << ' ';
-            break;
+            auto startIt = data.begin();
+            for (auto &row : rows)
+            {
+                row.removeImpossibleSequences(startIt);
+                row.paintKnownFields(startIt);
+                startIt += cols.size();
+            }
         }
-        std::cout << ' ';
+
+        {
+            size_t colId = 0;
+            for (auto &col : cols)
+            {
+                auto it = ColumnIterator<CellType, decltype(data.begin())>(data.begin() + colId,
+                                                                           cols.size());
+                col.removeImpossibleSequences(it);
+                col.paintKnownFields(it);
+
+                colId++;
+            }
+        }
+
+        for (size_t y = 0; y < rows.size(); ++y)
+        {
+            for (size_t x = 0; x < cols.size(); ++x)
+            {
+                auto cell = data[x + y * cols.size()];
+                switch (cell)
+                {
+                case E:
+                    std::cout << '_';
+                    break;
+                case B:
+                    std::cout << 'X';
+                    break;
+                case W:
+                    std::cout << ' ';
+                    break;
+                }
+                std::cout << ' ';
+            }
+            std::cout << '\n';
+        }
+
+        std::cout << "\n\n";
+
+        bool isAllRowsOk = std::all_of(rows.begin(), rows.end(),
+                                       [](auto &row) { return row.getSeqs().size() == 1; });
+        bool isAllColsOk = std::all_of(cols.begin(), cols.end(),
+                                       [](auto &col) { return col.getSeqs().size() == 1; });
+        if (isAllRowsOk && isAllColsOk)
+            break;
     }
-    std::cout << '\n';
 
     return 0;
 }
