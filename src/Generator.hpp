274e7251904708f259ab28a2a2bdc887cc1ec9ae
Sebastian Zientek <sebastian.zientek1@gmail.com>
2023-09-03 18:41:58 +0200

    Add assertion for Generator

--- src/Generator.hpp
+++ src/Generator.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cassert>
+#include <numeric>
 #include <vector>
 #include <numeric>
 
@@ -24,8 +26,12 @@ private:
     template <typename T>
     void init(T &&sideNumbers, size_t seqSize)
     {
+        auto blocksNum = std::max(sideNumbers.size(), static_cast<size_t>(1));
         size_t blocksWithSpaces
-            = std::accumulate(sideNumbers.begin(), sideNumbers.end(), sideNumbers.size() - 1);
+            = std::accumulate(sideNumbers.begin(), sideNumbers.end(), blocksNum - 1);
+
+        assert(blocksWithSpaces <= seqSize && "definied blocks are bigger than line size");
+
         size_t spacesAfterBlocks = seqSize - blocksWithSpaces;
         size_t numbers = sideNumbers.size() + spacesAfterBlocks;
         size_t length = sideNumbers.size();
